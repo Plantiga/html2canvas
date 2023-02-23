3a968720018c158d8e43de12a727c102a4b9e15e
Derek Ludwig <derek.s.ludwig@gmail.com>
2023-02-23 09:54:46 -0800

    fix: ensure text does not extend too far

--- src/render/canvas/canvas-renderer.ts
+++ src/render/canvas/canvas-renderer.ts
@@ -146,13 +146,13 @@ export class CanvasRenderer extends Renderer {
 
     renderTextWithLetterSpacing(text: TextBounds, letterSpacing: number, baseline: number): void {
         if (letterSpacing === 0) {
-            this.ctx.fillText(text.text, text.bounds.left, text.bounds.top + baseline);
+            this.ctx.fillText(text.text, text.bounds.left, text.bounds.top + baseline, text.bounds.width);
         } else {
             const letters = segmentGraphemes(text.text);
             letters.reduce((left, letter) => {
-                this.ctx.fillText(letter, left, text.bounds.top + baseline);
+                this.ctx.fillText(letter, left, text.bounds.top + baseline, text.bounds.width);
 
-                return left + this.ctx.measureText(letter).width;
+                return left + this.ctx.measureText(letter).width + letterSpacing;
             }, text.bounds.left);
         }
     }
